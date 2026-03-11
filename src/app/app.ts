1a4673a3aa7b4fbd503771256e9d5d44f46f78e1
Yesica Buitron <y.buitron@unimayor.edu.co>
2026-03-11 10:52:46 -0500

    Diseño de barra azul y tablas de escalas y resultados listo

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  template: `<router-outlet></router-outlet>`,
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
-export class App {}
+export class AppComponent { 
+  title = 'mi-proyecto-angular';
+}
--- src/app/pages/results/results.ts
+++ src/app/pages/results/results.ts
@@ -1,11 +1,45 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common'; // ESTO QUITA EL ERROR DE NGIF Y NGCLASS
+import { FormsModule } from '@angular/forms';   // ESTO QUITA EL ERROR DE NGMODEL
 
 @Component({
   selector: 'app-results',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule], // <--- ESTO ES LO MÁS IMPORTANTE
   templateUrl: './results.html',
-  styleUrl: './results.css',
+  styleUrls: ['./results.css']
 })
-export class Results {
+export class ResultsComponent {
+  // VARIABLES QUE EL HTML ESTÁ BUSCANDO
+  estudiante: string = '';
+  nota: number | null = null;
+  mostrarFormulario: boolean = false;
+  evaluacion: string = 'Matemáticas-Examen Final';
 
+  resultados = [
+    { estudiante: 'Juan perez', nota: 6.5 },
+    { estudiante: 'Ariana Gomez', nota: 6.4 },
+    { estudiante: 'jhoan muñoz', nota: 4.2 }
+  ];
+
+  // FUNCIONES QUE EL HTML ESTÁ BUSCANDO
+  abrirFormulario() {
+    this.mostrarFormulario = true;
+  }
+
+  guardarResultado() {
+    if (this.estudiante && this.nota !== null) {
+      this.resultados.push({
+        estudiante: this.estudiante,
+        nota: this.nota
+      });
+      this.cancelar();
+    }
+  }
+
+  cancelar() {
+    this.estudiante = '';
+    this.nota = null;
+    this.mostrarFormulario = false;
+  }
 }
