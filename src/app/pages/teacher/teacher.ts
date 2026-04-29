90e3f8f461ee1f6ceab89124d6488176a7cf0be2
spabonc@unimayor.edu.co <spabonc@unimayor.edu.co>
2026-04-29 11:38:41 -0500

    Remove

--- src/app/pages/teacher/teacher.ts
+++ src/app/pages/teacher/teacher.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ChangeDetectorRef } from '@angular/core'; // 🔥 agregado ChangeDetectorRef
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core'; 
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { RouterModule, Router } from '@angular/router';
